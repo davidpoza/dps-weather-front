6566515b24138b10761fe003a3cda29a4f2dac07
David Poza Suárez <pozasuarez@gmail.com>
2020-08-23 17:57:22 +0200

    added wrong data filter

--- src/components/chart/index.jsx
+++ src/components/chart/index.jsx
@@ -8,12 +8,22 @@ function Chart(props) {
   const { data1, data2 } = props;
   const classes = useStyles();
 
+  // removes wrong data which is out of certain range depending of type of data
+  const filterData = (data, type = 'temperature') => {
+    if (type === 'pressure') {
+      return data.filter((value) => (value.pressure > 700 && value.pressure < 1300));
+    }
+    if (type === 'humidity') {
+      return data.filter((value) => (value.humidity > 0 && value.humidity < 100));
+    }
+    return data.filter((value) => (value.temperature > -10 && value.temperature < 50));
+  };
 
   return (
     <div className={classes.root}>
       <ResponsiveLine
         colors={{ scheme: 'set1' }}
-        pointSize={10}
+        pointSize={4}
         curve="monotoneX"
         useMesh // interaction with mouse
         tooltip={(v) => `${v.point.data.yFormatted} a las ${v.point.data.xFormatted}`} // tooltip for interaction
@@ -23,14 +33,14 @@ function Chart(props) {
         data={[
           {
             id: 'Sensor1',
-            data: data1.slice(-15).map((value) => ({
+            data: filterData(data1, 'temperature').map((value) => ({
               x: value.created_on,
               y: value.temperature,
             })),
           },
           {
             id: 'Sensor2',
-            data: data2.slice(-15).map((value) => ({
+            data: filterData(data2, 'temperature').map((value) => ({
               x: value.created_on,
               y: value.temperature,
             })),
@@ -57,6 +67,19 @@ function Chart(props) {
           format: '%H:%M',
           tickValues: 'every hour',
         }}
+        legends={
+          [
+            {
+              anchor: 'bottom-right',
+              direction: 'column',
+              itemHeight: 20,
+              itemWidth: 80,
+              translateX: 0, // px
+              translateY: -40, // px
+              justify: true,
+            },
+          ]
+        }
       />
     </div>
   );
