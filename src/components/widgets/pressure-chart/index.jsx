de9f9ff5ea4784e1d6a574cff603dea199e1ca3d
David Poza Suárez <pozasuarez@gmail.com>
2021-06-17 19:50:53 +0200

    pressure read from indoor

--- src/components/widgets/pressure-chart/index.jsx
+++ src/components/widgets/pressure-chart/index.jsx
@@ -22,7 +22,7 @@ export default function TemperatureChart() {
         charts={[
           {
             sensorId: '',
-            data: state.graphs?.HOME_OUTDOOR?.[date],
+            data: state.graphs?.HOME_INDOOR?.[date],
           },
         ]}
         date={date}
